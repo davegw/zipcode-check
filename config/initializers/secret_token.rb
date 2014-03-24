9f4080cd2371319be274408f2c993ffeb53f2aea
davidgertmenianwong <david@giftrocket.com>
2014-03-24 01:02:04 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Zenefitstest::Application.config.secret_key_base = '3bc6beee9982f5b1edc0bd7be01ef52a945f2bc40d9565795158c591d5fd9f3f22da69b473ce0487bd2f51274eb8262aa397ef62bad62afb2e898a3f2d9ac1e6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Zenefitstest::Application.config.session_store :cookie_store, key: '_zenefitstest_session'
