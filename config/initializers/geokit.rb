bf87b778093494171b71dc025b95c55a74895c94
davidgertmenianwong <david@giftrocket.com>
2014-03-26 22:51:19 -0700

    add geokit initializer2

--- config/initializers/geokit.rb
+++ config/initializers/geokit.rb
@@ -1,3 +1,6 @@
+require 'rubygems'
 require 'geokit'
 
+
+Geokit::Geocoders::Google3Geocoder = Geokit::Geocoders::GoogleGeocoder3
 GeoKit::Geocoders::google = 'AIzaSyBHB-4XsqFcIYYhid36PjMj5YJwkiFYy7Y'
